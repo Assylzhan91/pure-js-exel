c9d550557ccafe7a44e1cb1507e2908dd2074510
Assylzhan91 <asylzhan.baimuratov@gmail.com>
2020-05-20 15:30:29 +0600

    finish project config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './index.html'
 import './styles/styles.scss'
 console.log('Hello, World')
+
+async function start() {
+    return await new Promise('start')
+}
+start().then(console.log)
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,10 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const path = require('path')
-
 const isProd = process.env.NODE_ENV === 'production'
 const isDev = !isProd
-
 console.log('isProd', isProd)
 console.log('isDev', isDev)
 
@@ -23,22 +21,36 @@ module.exports = {
     devServer: {
         port: 9991,
         contentBase: './dist',
-        open: true
+        open: true,
+        hot: isDev,
     },
     module: {
         rules: [
             {
                 test: /\.m?js$/,
                 exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                }
+                use: [
+                    {
+                        loader: 'babel-loader',
+                    },
+
+                    {
+                        loader: isDev ? 'eslint-loader' : null,
+                    },
+
+                ],
             },
 
             {
                 test: /\.s[ac]ss$/i,
                 use: [
-                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: isDev,
+                            reloadAll: true,
+                        },
+                    },
                     'css-loader',
                     {
                         loader: 'sass-loader',
@@ -46,22 +58,23 @@ module.exports = {
                             sourceMap: true,
                             sassOptions: {
                                 outputStyle: 'compressed',
-                            }
-                        }
-                    }
+                            },
+                        },
+                    },
                 ],
-            },
+            }, //SCSS
+
             {
                 test: /\.html$/,
                 use: [
                     {
-                        loader: "html-loader",
+                        loader: 'html-loader',
                         options: {
                             minimize: true,
                         },
-                    }
-                ]
-            }
+                    },
+                ],
+            }, //HTML
         ],
     },
     resolve: {
@@ -69,28 +82,28 @@ module.exports = {
         alias: {
             '@': path.join(__dirname, 'src'),
             '@core': path.join(__dirname, 'src/core'),
-        }
+        },
     },
+
     plugins: [
         new HtmlWebpackPlugin({
         template: './src/index.html',
-        title : 'Weather Forecaster',
+        title: 'Weather Forecaster',
         minify: {
             removeComments: isProd,
             collapseWhitespace: isProd,
-        }
+        },
         }),
         new CleanWebpackPlugin(),
         new CopyPlugin({
             patterns: [
-                { from: path.join(__dirname, 'src/js-icon-48x48.ico'),
-                    to: path.join(__dirname, 'dist')
-                }
+                {from: path.join(__dirname, 'src/js-icon-48x48.ico'),
+                    to: path.join(__dirname, 'dist'),
+                },
             ],
         }),
         new MiniCssExtractPlugin({
-            filename: isDev ? 'bundle.css' : 'bundle.[hash].css'
-        })
+            filename: isDev ? 'bundle.css' : 'bundle.[hash].css',
+        }),
     ],
-
-};
+}
