fa37ec8766a954f11517ad590f37891cfee93bdf
Assylzhan91 <asylzhan.baimuratov@gmail.com>
2020-05-24 13:17:44 +0600

    lesson 38

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -8,28 +8,32 @@ function toChar(_, index) {
 }
 
 export function createTable(rowsCount = 15) {
-    const colCount = charCodeAt.Z - charCodeAt.A + 1
+    const colsCount = charCodeAt.Z - charCodeAt.A + 1
     const rows = []
-    const columns = new Array(colCount)
+    const columns = new Array(colsCount)
         .fill('')
         .map(toChar)
         .map(toColumn)
         .join('')
 
     rows.push(createRow(columns))
-    for (let i = 1; i < rowsCount; i++) {
-        rows.push(createRow())
+    for (let i = 0; i < rowsCount; i++) {
+        const cells = new Array(colsCount)
+            .fill('')
+            .map(toCell)
+            .join('')
+        rows.push(createRow(cells, i+1))
     }
     return rows.join('')
 }
 
-function createRow(content) {
+function createRow(content, num = '') {
     return `<div class="row">
-        <div class="row-info"></div>
+        <div class="row-info">${num}</div>
         <div class="row-data">${content}</div>
       </div>`
 }
 
 const toColumn = (col) =>`<div class="column">${col}</div>`
 
-const createCell = ()=> `<div class="cell selected" contenteditable>A1</div>`
+const toCell = ()=> `<div class="cell" contenteditable></div>`
