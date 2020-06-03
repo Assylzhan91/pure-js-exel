130a4bc3e41e56835168a5d10f0c5cc1c1a3a9fb
Assylzhan91 <asylzhan.baimuratov@gmail.com>
2020-06-04 03:40:11 +0600

    lesson part=01 49

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -16,35 +16,36 @@ export class Table extends ExcelComponent {
 
     onMousedown(event) {
         if (event.target.dataset.resize) {
+            document.onselectstart = (e)=> e.preventDefault()
             const $target = $(event.target)
             const $parent = $target.closest('[data-type="resizable"]')
             const coords = $parent.getCoords()
-            const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`)
             const type = $target.data.resize
-            // $parent.css({
-            //     color: 'red',
-            //     backgroundColor: 'green',
-            //     borderColor: 'blue',
-            // })
+            let value
+            $target.css({opacity: 1, zIndex: 1000, bottom: '-5000px'})
             const mouseMoveHandler = (e) => {
                 if (type === 'col') {
                     const delta = e.pageX - coords.right
-                    const value = coords.width + delta
-                    $parent.css({width: value + 'px'})
-                    cells.forEach((item)=>{
-                        $(item).css({width: value + 'px'})
-                    })
+                    value = coords.width + delta
+                    $target.css({right: -delta + 'px'})
                 } else {
                     const diff = e.pageY - coords.bottom
                     $parent.css({height: coords.height + diff + 'px'})
                 }
             }
 
-            document.addEventListener('mousemove', mouseMoveHandler)
-
-            document.addEventListener('mouseup', (e)=>{
+            const removeMouseDown = ()=> {
+                if (type === 'col') {
+                    $parent.css({width: value + 'px'})
+                    this.$root.findAll(`[data-col="${$parent.data.col}"]`)
+                        .forEach((item)=>item.style.width = value + 'px')
+                }
                 document.removeEventListener('mousemove', mouseMoveHandler)
-            })
+                document.removeEventListener('mouseup', removeMouseDown)
+                $target.$el.removeAttribute('style')
+            }
+            document.addEventListener('mousemove', mouseMoveHandler)
+            document.addEventListener('mouseup', removeMouseDown)
         }
     }
 }
